0aa800f093e908f8f1e9c2230adb237db363f865
Esm322 <86346824+Esm322@users.noreply.github.com>
2024-03-22 17:37:03 +0300

    made layout with styles for components CalendarRu and CalendarEng

---
+++ src/components/CalendarEng.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="calendar">
+    <div class="calendar__header">
+      <button class="calendar__btn-pre btn-reset">
+        <ArrowDicrementSVG/>
+      </button>
+      <span class="calendar__date">text</span>
+      <button class="calendar__btn-next btn-reset">
+        <ArrowIncrementSVG/>
+      </button>
+    </div>
+    <ul class="list calendar__names-day list-reset">
+      <li class="list__item-name">
+        weekday
+      </li>
+    </ul>
+    <ul class="list calendar__days list-reset">
+      <li class="list__item-day" v-for="day in 31" :key="day">
+        {{ day }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import ArrowDicrementSVG from './componentsSVG/ArrowDicrementSVG.vue';
+import ArrowIncrementSVG from './componentsSVG/ArrowIncrementSVG.vue';
+</script>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <button class="btn-switch btn-reset">click</button>
+
+    <CalendarRu/>
+
+    <CalendarEng/>
+  </div>
+</template>
+
+<script setup>
+import CalendarRu from '@/components/CalendarRu.vue';
+import CalendarEng from '@/components/CalendarEng.vue';
+</script>
+
+<style lang="scss">
+html {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+*,
+::before,
+::after {
+  box-sizing: inherit;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1110px;
+  height: 98vh;
+
+  .calendar {
+    width: 50%;
+    margin: 0 auto;
+    padding: 10px;
+    transform: translateY(10vh);
+    border: 1px solid #000;
+    border-radius: 5px;
+
+    .calendar__header {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px 25px;
+      border-bottom: 1px solid #000;
+
+      .calendar__date {
+        font-size: 20px;
+      }
+    }
+
+    .calendar__names-day {
+      display: flex;
+
+      .list__item-name {
+        padding: 10px;
+        width: calc(100% / 7);
+        text-align: center;
+      }
+    }
+
+    .calendar__days {
+      display: flex;
+      flex-wrap: wrap;
+
+      .list__item-day {
+        padding: 10px;
+        width: calc(100% / 7);
+        color: #bcbcbc;
+        text-align: center;
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+.btn-reset {
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.btn-switch {
+  display: block;
+  margin: 0 auto;
+  padding: 10px;
+  border: 1px solid #1061d1;
+  color: #1061d1;
+  transform: translateY(40%);
+  transition: background-color .3s ease-in-out, color .3s ease-in-out;
+}
+
+.btn-switch:hover{
+  background-color: #1061d1;
+  color: #fff;
+}
+
+.list-reset {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+</style>
